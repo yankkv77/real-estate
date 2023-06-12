91571bfb8195a1ccc79863f4f2e543ce5648941e
yankkv83 <yankkv83@gmail.com>
2023-06-12 23:34:06 +0300

    fix: replace HeroSection from App.js to HomeScreen
    The change improves code organization and establishes a more logical
    separation of concerns.

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,19 +1,30 @@
 import React from "react";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Container } from "react-bootstrap";
 import properties from "../properties";
 import Property from "../components/Property";
+import HeroSection from "../components/HeroSection";
 
 function HomeScreen() {
     return (
-        <div className="py-3">
-            <h2>Popular Properties</h2>
-            <Row>
-                {properties.map((property) => (
-                    <Col className="py-3" key={property._id} sm={1} md={6} lg={5} xl={3}>
-                        <Property  property={property} />
-                    </Col>
-                ))}
-            </Row>
+        <div>
+            <HeroSection />
+            <Container className="py-3">
+                <h2>Popular Properties</h2>
+                <Row>
+                    {properties.map((property) => (
+                        <Col
+                            className="py-3"
+                            key={property._id}
+                            sm={1}
+                            md={6}
+                            lg={5}
+                            xl={3}
+                        >
+                            <Property property={property} />
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
         </div>
     );
 }
